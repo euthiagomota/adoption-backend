c537c1af215246c1f9500570ccdde10b172985b7
Thiago Mota <euthiagojose2004@gmail.com>
2025-05-11 18:03:46 -0300

    fix: find router connected on frontend

--- src/main/java/com/backend/adoption/config/SecurityConfig.java
+++ src/main/java/com/backend/adoption/config/SecurityConfig.java
@@ -25,6 +25,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
@@ -37,6 +39,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf(csrf -> csrf.disable()) // Desativa CSRF (útil para APIs REST)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
